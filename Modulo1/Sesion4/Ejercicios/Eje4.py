9f0528310ce189bb5cd0e7245f2b7254832aeac7
marcelosanchez166 <marcelosanchez166@gmail.com>
2023-03-27 21:32:09 -0600

    continuacion de Curso

--- Modulo1/Sesion4/Ejercicios/Eje4.py
+++ Modulo1/Sesion4/Ejercicios/Eje4.py
@@ -5,12 +5,35 @@
 # total a pagar final de la cesta.
 
 
-def descuentos(precio,descuentos):
-    desc=precio*descuentos
+def aplicar_descuento(precio,descuento):
+    return precio * (1 - descuento / 100)
 
-def IVA():
-    pass
+def aplicar_iva(precio,iva):
+    return precio * (1 + iva / 100)
 
 
-def Cesta_Compra():
-    pass
+def calcular_total(cesta, descuento, iva):
+    """
+    Calcula el total a pagar por una cesta de la compra después de aplicar descuentos e IVA.
+
+    Args:
+        cesta (dict): Un diccionario con los nombres de los productos como claves y los precios como valores.
+        descuento (float): El porcentaje de descuento a aplicar a los precios de la cesta.
+        iva (float): El porcentaje de IVA a aplicar a los precios de la cesta después de aplicar el descuento.
+
+    Returns:
+        float: El total a pagar por la cesta después de aplicar descuentos e IVA.
+    """
+    total = 0
+    for producto, precio in cesta.items():
+        precio_descuento = aplicar_descuento(precio, descuento)
+        precio_iva = aplicar_iva(precio_descuento, iva)
+        total += precio_iva
+    return total
+
+cesta={"papas":1.24,"frijoles":0.5, "maiz":1.5,"Elotes":1.00,"Tomates":0.9}
+print(calcular_total(cesta,0.10,0.13))
+
+
+
+
